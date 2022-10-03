172943984c5920805eb115d6a3df7e0096b3d9ec
cyrox007 <cyrox007@gmail.com>
2022-10-03 20:04:25 +0300

    fix bug

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 let interval;
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
-let x = canvas.width/2;
-let y = canvas.height-30;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 let ballRadius = 10;
 let paddleHeight = 10;
 let paddleWidth = 75;
-let paddleX = (canvas.width-paddleWidth)/2;
+let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
 let drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
@@ -26,10 +26,12 @@ let draw = () => {
     drawPaddle();
     x += dx;
     y += dy;
+
     if (y + dy < ballRadius) {
         dy = -dy;
-    } else if (y + dy > canvas.height-ballRadius) {
-        if (x > paddleX && x < paddleX - paddleWidth) {
+    } else if (y + dy > canvas.height - ballRadius) {
+        
+        if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
         } else {
             alert('Game Over!');
@@ -37,11 +39,11 @@ let draw = () => {
             clearInterval(interval);
         }
     }
-    if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
+    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx;
     }
 
-    if (rightPressed && paddleX < canvas.width-paddleWidth) {
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
     } else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
